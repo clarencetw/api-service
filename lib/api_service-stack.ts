d6b975e0e9c3561a7d9d13eaf676b06b22b434a8
Clarence <me@clarence.tw>
2021-06-16 00:43:14 +0800

    Add lambda use inline

--- lib/api_service-stack.ts
+++ lib/api_service-stack.ts
@@ -1,9 +1,26 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
 export class ApiServiceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const main = new lambda.Function(this, "lambda", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: "index.handler",
+      code: lambda.Code.fromInline(' \
+        exports.handler = async function (event) { \
+          return { \
+            statusCode: 200, \
+            headers: { "Content-Type": "application/json" }, \
+            body: JSON.stringify(event), \
+          }; \
+        };')
+    });
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: main
+    })
   }
 }

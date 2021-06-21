3c49ac40caf8e1e87faed6d2b58745c9ed30328b
Clarence <me@clarence.tw>
2021-06-21 17:34:07 +0800

    Update api gateway use multi lambda file

--- lib/api_service-stack.ts
+++ lib/api_service-stack.ts
@@ -6,21 +6,26 @@ export class ApiServiceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const api = new apigw.RestApi(this, "Endpoint");
+
     const main = new lambda.Function(this, "lambda", {
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: "index.handler",
       code: lambda.Code.fromAsset("resources")
     });
+    const getIntegration = new apigw.LambdaIntegration(main);
+    api.root.addMethod("GET", getIntegration);
 
-    const api = new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: main,
-      proxy: false
-    })
-
-    const root = api.root;
-    root.addMethod('GET')
-
+    const helloHandler = new lambda.Function(this, "helloLambda", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: "hello.handler",
+      code: lambda.Code.fromAsset("resources")
+    });
+    const getHelloIntegration = new apigw.LambdaIntegration(helloHandler);
     const hello = api.root.addResource('hello');
-    hello.addMethod('GET'); 
+    hello.addMethod('GET', getHelloIntegration);
+
+    const aws = api.root.addResource('example');
+    aws.addMethod('GET', new apigw.HttpIntegration('https://example.com/'));
   }
 }
--- resources/index.js
+++ resources/index.js
@@ -1,17 +1,7 @@
 exports.handler = async function (event) {
-    if (event.httpMethod === "GET") {
-        if (event.path === "/") {
-            return {
-                statusCode: 200,
-                headers: { "Content-Type": "application/json" },
-                body: `{"name":"Hello AWS CDK!"}`
-            };
-        }
-    }
-
-    return { 
-        statusCode: 200, 
-        headers: { "Content-Type": "application/json" }, 
-        body: JSON.stringify(event), 
-    }; 
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: `{"name":"Hello AWS CDK!"}`
+    };
 };
